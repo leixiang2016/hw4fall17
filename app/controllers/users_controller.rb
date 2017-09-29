86140b19cc32369c8247efeda6b524d08357b963
LeiXiang <lei-xiang@uiowa.edu>
2017-09-29 17:59:19 +0000

    message of your choosing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,15 @@
 class UsersController < ApplicationController
 
   def new
+    # default: render 'new' template
   end
 
   def create
     @user=User.create_user!(params[:user])
 
     if !!(@user)
-      flash[:notice] = "New user #{@user.user_id} was successfully created."
-      redirect_to user_path
+      flash[:notice] = "Welcome #{@user.user_id}. Your account has been created."
+      redirect_to movies_path
     else
       flash[:notice] = "The User Id #{params[:user][:user_id]} Sorry, this user_id is taken. Try abain."
       redirect_to new_user_path
