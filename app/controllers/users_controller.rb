37fdd29cb9170527674dd094205d56d874474571
LeiXiang <lei-xiang@uiowa.edu>
2017-09-29 18:08:05 +0000

    message of your choosing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,14 @@
 class UsersController < ApplicationController
 
+  def user_params
+    params.require(:user).permit(:user_id, :email, :session_token)
+  end
   def new
     # default: render 'new' template
   end
 
   def create
-    @user=User.create_user!(params[:user])
+    @user=User.create_user!(user_params)
 
     if !!(@user)
       flash[:notice] = "Welcome #{@user.user_id}. Your account has been created."
