6c6953cc4b39c2983dbd1923d0e75d271a98215f
LeiXiang <lei-xiang@uiowa.edu>
2017-09-29 18:20:51 +0000

    message of your choosing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user=User.create_user!(user_params)
     flash[:notice] = "Welcome #{@user.user_id}. Your account has been created."
-    redirect_to movies_path
+    redirect_to new_user_path
     
     
 
