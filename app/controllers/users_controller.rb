2ff65b56c62d7c2e7e369698c85983afc42425f7
LeiXiang <lei-xiang@uiowa.edu>
2017-09-30 02:17:59 +0000

    message of your choosing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,19 +3,20 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:user_id, :email, :session_token)
   end
+  
   def new
-    # default: render 'new' template
+    # default: render 'new' templates
   end
 
   def create
-    if User.exists?(user_id)
-      flash[:notice] = "The User Id #{params[:user][:user_id]} Sorry, this user_id is taken. Try abain."
+    if User.find_by_user_id(params[:user][:user_id])
+      flash[:notice]= "Sorry, this user-id is taken. Try again."
       redirect_to new_user_path
-    else
-      @user = User.create!(user_params)
+    else   
+      @user = User.create_user!(user_params)
       flash[:notice] = "Welcome #{@user.user_id}. Your account has been created."
-      redirect_to root_path
-    end
+      redirect_to login_path
+    end  
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  def self.create_user!(arg)
+    self.create!(arg.merge({:session_token => SecureRandom.base64}))
+  end
 end
