9d35badac801cceb84ab97f95b1833583962b804
LeiXiang <lei-xiang@uiowa.edu>
2017-09-30 04:54:53 +0000

    message of your choosing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,13 +10,12 @@ class UsersController < ApplicationController
 
   def create
     if User.find_by_user_id(params[:user][:user_id])
-      flash[:notice]= 'Sorry, this user-id is taken. Try again.'
-      flash[:class]='flashMessage'
+      flash[:notice]= {:class => :flashMessage, :body =>'Sorry, this user-id is taken. Try again.'}
+
       redirect_to new_user_path
     else   
       @user = User.create_user!(user_params)
-      flash[:notice] = "Welcome #{@user.user_id}. Your account has been created."
-      flash[:class]='flashMessage'
+      flash[:notice] = {:class => :flashMessage, :body =>"Welcome #{@user.user_id}. Your account has been created."}
       redirect_to login_path
     end  
   end
