ce9dd121d4d3fa253340a669d0a21da82683ae18
LeiXiang <lei-xiang@uiowa.edu>
2017-10-01 19:44:49 +0000

    message of your choosing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
 
   def create
     if User.find_by_user_id(params[:user][:user_id])
-      flash[:notice][]= {:class => :flashMessage, :body => 'Sorry, this user-id is taken. Try again.'}
+      flash[:notice]= {:class => :flashMessage, :body => 'Sorry, this user-id is taken. Try again.'}
       redirect_to new_user_path
     else   
       @user = User.create_user!(user_params)
